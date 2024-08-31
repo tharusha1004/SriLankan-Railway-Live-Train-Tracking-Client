f10cbf5a8ee08b744280c48beb4867f928306d51
tharusha1004 <tharusha.astro@gmail.com>
2024-08-31 19:33:53 +0530

    implement the app.js file with necessary import pages

--- train-tracking-app/src/App.js
+++ train-tracking-app/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SelectionPage from './Pages/UserSelectionPage';
+import LoginPage from './Pages/Auth/LoginPage';
+import RegisterPage from './Pages/Auth/RegisterPage';
+import AdminDashboard from './Pages/AdminDashboard';
+import UserDashboard from './Pages/UserDashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SelectionPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/user-dashboard" element={<UserDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
